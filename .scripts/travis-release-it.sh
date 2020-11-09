78dbe2c2b42e4eb097c13b764b4f4cf290cdcaeb
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2020-11-09 18:13:53 +0200

    fix: release reset debug - v6

--- .scripts/travis-release-it.sh
+++ .scripts/travis-release-it.sh
@@ -7,8 +7,6 @@ git config user.name "Travis CI"
 git remote rm origin
 git remote add origin https://${GH_TOKEN}@github.com/mists-aside/nestjs-metrics.git
 git symbolic-ref HEAD refs/heads/master
-# git checkout master
-# git branch --set-upstream-to=origin/master master
 
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
 
@@ -16,4 +14,4 @@ git status
 
 npm run release -- patch --no-git.requireUpstream #--dry-run
 
-npm release
+npm publish
